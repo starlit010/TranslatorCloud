a3ca3ecf4063863ec1a5d823f44d922eeb48d514
starlit010 <starlit010@hotmail.com>
2018-10-17 17:25:11 +0800

    初次提交

---
+++ translator-cloud-web/src/main/java/com/grayliu/translator/OurtranslatorApplication.java
@@ -0,0 +1,12 @@
+package com.grayliu.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurtranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurtranslatorApplication.class, args);
+	}
+}
